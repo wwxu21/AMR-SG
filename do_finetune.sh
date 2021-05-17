b4809f60c2fab4c00a0c35eac639589eb82085b8
wwxu21 <weiwen.xuu@gmail.com>
2021-05-17 15:54:55 +0800

    code for AMR-SG

--- do_finetune.sh
+++ do_finetune.sh
@@ -1,4 +1,4 @@
  python3 ./RoBERTa_finetune/finetune.py \
   --task_name openbook --model_name roberta-large --data_path obqa_preprocess \
   --max_len 256 --do_train --do_eval --do_test --learning_rate 2e-5\
-  --num_train_epochs 8 --transition_number 2 --fp16
+  --num_train_epochs 8 --transition_number 2
